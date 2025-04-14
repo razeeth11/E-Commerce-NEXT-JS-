2b61fa3a4f61e2ee24619d9522d4d5e0313ba257
razeeth11 <abrazeeth@gmail.com>
2025-04-14 23:08:56 +0530

    Folder-Structure-Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div>
-      <div>
-        <h1 className="text-2xl">Hello World!</h1>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
